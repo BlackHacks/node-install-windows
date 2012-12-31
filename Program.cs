5de836324688b9cd30d3683db2eb51e19e57b2f1
Ayman Farhat <ayman.farhat1@gmail.com>
2012-12-31 17:21:34 +0200

    Download Node function added

--- Program.cs
+++ Program.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Net;
+using System.ComponentModel;
+using System.Threading;
 
 namespace node_install_windows
 {
@@ -9,7 +11,23 @@ namespace node_install_windows
     {
         static void Main(string[] args)
         {
-           
+            Console.Write("Downloading NodeJS...");
+            getNode("64", "0.8.16");
+        }
+        
+        private static void getNode(string architecture, string version)
+        {
+            WebClient client = new WebClient();
+
+            string url = String.Format("http://nodejs.org/dist/v{0}/x{1}/node-v{0}-x{1}.msi", version, architecture);
+
+            string savePath = String.Format("node-v{0}-x{1}.msi", version, architecture);
+
+            try
+            {
+                client.DownloadFile(url, savePath);
+            }
+            catch (Exception e) { Console.WriteLine(e.StackTrace); }
         }
     }
 }

f424d27fb13645a7b7b75202373182571aa3faba
Ayman Farhat <ayman.farhat1@gmail.com>
2012-12-31 17:37:27 +0200

    Auto detect system architecture

--- Program.cs
+++ Program.cs
@@ -12,11 +12,15 @@ namespace node_install_windows
         static void Main(string[] args)
         {
             Console.Write("Downloading NodeJS...");
-            getNode("64", "0.8.16");
+            Console.WriteLine();
+            
+            getNode("0.8.16");
         }
         
-        private static void getNode(string architecture, string version)
+        private static void getNode(string version, string architecture="")
         {
+            architecture = (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))) ? "64" : "86";
+
             WebClient client = new WebClient();
 
             string url = String.Format("http://nodejs.org/dist/v{0}/x{1}/node-v{0}-x{1}.msi", version, architecture);

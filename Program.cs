6439dd6c2950b05bf489a942817aebed745a4b36
Ayman Farhat <ayman.farhat1@gmail.com>
2013-01-01 10:44:58 +0200

    Invoke Node installer and wait wizard to finish

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ using System.ComponentModel;
 using System.Threading;
 using System.IO;
 using System.Text.RegularExpressions;
+using System.Diagnostics;
 
 namespace node_install_windows
 {
@@ -13,12 +14,27 @@ namespace node_install_windows
     {
         static void Main(string[] args)
         {
-            Console.Write("Downloading NodeJS...");            
-            getNode();
+            Console.Write("Downloading NodeJS...");       
+            
+            /* Download NodeJS and get the file name */
+            string fname = getNode();
+
+            Console.WriteLine("Installing...");
+            
+            /* Invoke the installer and wait for it to finish */
+            Process nodeInstallPr = new Process();
+
+            nodeInstallPr.StartInfo.FileName = fname;
+            nodeInstallPr.Start();
+            nodeInstallPr.WaitForInputIdle();
+            nodeInstallPr.WaitForExit();
+            nodeInstallPr.Close();
+
+            Console.WriteLine("Node installed!");
         }
         
-        /* Downloads nodeJS*/
-        private static void getNode(string version=null, string architecture=null)
+        /* Downloads nodeJS returns the file name downloaded */
+        private static string getNode(string version=null, string architecture=null)
         {
             if(architecture == null)
                 architecture = (!string.IsNullOrEmpty(Environment.GetEnvironmentVariable("PROCESSOR_ARCHITEW6432"))) ? "64" : "86";
@@ -37,6 +53,8 @@ namespace node_install_windows
                 client.DownloadFile(url, savePath);
             }
             catch (Exception e) { Console.WriteLine(e.StackTrace); }
+
+            return savePath;
         }
 
         /* Scrapes http://nodejs.org/dist/latest/ and identifies the version number of the latest stable release */
